f946998e4049ee40ada6c0f8d0774d0894b59465
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-11-20 19:49:24 -0600

    added gitignore

---
+++ FRESCO/FRESCO/Colors/ColorsExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorsExtension.swift
+//  FRESCO
+//
+//  Created by Néstor I. Martínez Ostoa on 20/11/19.
+//  Copyright © 2019 Néstor I. Martínez Ostoa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let customOrange = UIColor(red: 232/255, green: 90/255, blue: 70/255, alpha: 1)
+}
+
---
+++ FRESCO/FRESCO/FCA/Models/FCA.swift
@@ -0,0 +1,9 @@
+//
+//  FCA.swift
+//  FRESCO
+//
+//  Created by Néstor I. Martínez Ostoa on 20/11/19.
+//  Copyright © 2019 Néstor I. Martínez Ostoa. All rights reserved.
+//
+
+import Foundation
